a939832d9434d17b77fd2a8fd69d26e65cb7764c
Bianca <biancabezerrathe71@gmail.com>
2024-10-22 21:28:18 -0300

    feat: adição do whitenoise

--- djangoapp/patrocars/settings.py
+++ djangoapp/patrocars/settings.py
@@ -50,10 +50,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_ulid",
     "patrocrud",
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
